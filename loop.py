b86b49dfee5a02348e1ab5b8e981c0703f342760
degr8sid-code <58777694+degr8sid-code@users.noreply.github.com>
2020-07-29 21:43:42 +0500

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,15 @@
+#Example Number 3: For and While Loop
+
+words = ['one', 'two', 'three', 'four', 'five']
+
+#while loop
+
+n = 0
+while(n < 5):
+    print(words[n])
+    n += 1
+    
+#for loop
+
+for i in words:
+    print(i)
